e320a81fb09f7ed78f22086ec5c034cc203fc242
apodisation13 <apodisation13@gmail.com>
2026-01-12 10:03:39 +0300

    db loaded

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import "vue-toastification/dist/index.css"
 import Vue3TouchEvents from "vue3-touch-events"
 import VueKonva from "vue-konva"
 import VueFullscreen from "vue-fullscreen"
-import * as Sentry from "@sentry/vue"
-import { BrowserTracing } from "@sentry/tracing"
+// import * as Sentry from "@sentry/vue"
+// import { BrowserTracing } from "@sentry/tracing"
 
 const app = createApp(App)
 
@@ -17,19 +17,21 @@ const toastOptions = {
   timeout: 2000,
 }
 
-if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    app,
-    dsn: "https://b8e6f199e75f43d1973e376cf7ab0c74@o4504139249876992.ingest.sentry.io/4504140574162944",
-    integrations: [
-      new BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-        tracingOrigins: ["*"],
-      }),
-    ],
-    tracesSampleRate: 1.0,
-  })
-}
+// if (process.env.NODE_ENV === "production") {
+//   Sentry.init({
+//     app,
+//     dsn: "https://b8e6f199e75f43d1973e376cf7ab0c74@o4504139249876992.ingest.sentry.io/4504140574162944",
+//     integrations: [
+//       new BrowserTracing({
+//         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+//         tracingOrigins: ["*"],
+//       }),
+//     ],
+//     tracesSampleRate: 1.0,
+//   })
+// }
+
+app.config.devtools = true
 
 app
   .use(router)
