4e56a3ba9abfea9bc6d401cec04f13185cf3e5a9
apodisation13 <apodisation13@gmail.com>
2026-02-06 16:04:09 +0300

    card abilities models

--- src/components/UI/CardsUI/Cards/AbilityCircleCard.vue
+++ src/components/UI/CardsUI/Cards/AbilityCircleCard.vue
@@ -1,49 +1,58 @@
 <template>
   <div>
     <!--  Кружок абилок карт игрока и лидера игрока-->
-    <heal-ability v-if="card.ability.name === 'heal'" :heal="card.heal" />
+    <heal-ability
+      v-if="card.ability.name === CardAbility.Heal"
+      :heal="card.heal"
+    />
 
     <!--Дополнительные способности карт: урон на строку, столбец, всем, распределить, уничтожить, заблочить-->
-    <row-attack-ability v-if="card.ability.name === 'damage-row'" />
-    <column-attack-ability v-else-if="card.ability.name === 'damage-column'" />
-    <all-attack-ability v-else-if="card.ability.name === 'damage-all'" />
-    <spread-attack-ability v-else-if="card.ability.name === 'spread-damage'" />
-    <lock-ability v-else-if="card.ability.name === 'lock'" />
+    <row-attack-ability v-if="card.ability.name === CardAbility.DamageRow" />
+    <column-attack-ability
+      v-else-if="card.ability.name === CardAbility.DamageColumn"
+    />
+    <all-attack-ability
+      v-else-if="card.ability.name === CardAbility.DamageAll"
+    />
+    <spread-attack-ability
+      v-else-if="card.ability.name === CardAbility.SpreadDamage"
+    />
+    <lock-ability v-else-if="card.ability.name === CardAbility.Lock" />
     <destroy-ability
       v-else-if="
-        card.ability.name === 'destroy-highest-hp' ||
-        card.ability.name === 'destroy-highest-damage' ||
-        card.ability.name === 'destroy-random' ||
-        card.ability.name === 'destroy-all-same-hp' ||
-        card.ability.name === 'destroy-random-enemy-in-deck'
+        card.ability.name === CardAbility.DestroyHighestHp ||
+        card.ability.name === CardAbility.DestroyHighestDamage ||
+        card.ability.name === CardAbility.DestroyRandom ||
+        card.ability.name === CardAbility.DestroyAllSameHp ||
+        card.ability.name === CardAbility.DestroyRandomEnemyInDeck
       "
     />
     <card-interaction-ability
       v-else-if="
-        card.ability.name === 'resurrect' ||
-        card.ability.name === 'draw-two-cards' ||
-        card.ability.name === 'give-charges-to-card-in-hand-1' ||
-        card.ability.name === 'play-from-deck' ||
-        card.ability.name === 'discard-draw-2' ||
-        card.ability.name === 'play-from-grave' ||
-        card.ability.name === 'incr-dmg-to-hand-by-self-dmg' ||
-        card.ability.name === 'play-enemy-from-grave' ||
-        card.ability.name === 'play-special-from-deck' ||
-        card.ability.name === 'play-special-from-grave' ||
-        card.ability.name === 'decr-dmg-to-hand-incr-to-random-hand' ||
-        card.ability.name === 'incr-dmg-by-n-charges' ||
-        card.ability.name === 'create-special' ||
-        card.ability.name === 'create-any-unit' ||
-        card.ability.name === 'create-and-put-to-deck' ||
-        card.ability.name === 'draw-exact'
+        card.ability.name === CardAbility.Resurrect ||
+        card.ability.name === CardAbility.DrawTwoCards ||
+        card.ability.name === CardAbility.GiveChargesToCardInHand1 ||
+        card.ability.name === CardAbility.PlayFromDeck ||
+        card.ability.name === CardAbility.DiscardDraw2 ||
+        card.ability.name === CardAbility.PlayFromDeck ||
+        card.ability.name === CardAbility.IncrDmgToHandBySelfDmg ||
+        card.ability.name === CardAbility.PlayEnemyFromGrave ||
+        card.ability.name === CardAbility.PlaySpecialFromDeck ||
+        card.ability.name === CardAbility.PlaySpecialFromGrave ||
+        card.ability.name === CardAbility.DecrDmgToHandIncrToRandomHand ||
+        card.ability.name === CardAbility.IncrDmgByNCharges ||
+        card.ability.name === CardAbility.CreateSpecial ||
+        card.ability.name === CardAbility.CreateAnyUnit ||
+        card.ability.name === CardAbility.CreateAndPutToDeck ||
+        card.ability.name === CardAbility.DrawExact
       "
     />
     <field-interaction-ability
       v-else-if="
-        card.ability.name === 'move-enemy' ||
-        card.ability.name === 'set-enemy-as-token' ||
-        card.ability.name === 'place-self-in-field' ||
-        card.ability.name === 'spawn-effect-in-row'
+        card.ability.name === CardAbility.MoveEnemy ||
+        card.ability.name === CardAbility.SetEnemyAsToken ||
+        card.ability.name === CardAbility.PlaceSelfInField ||
+        card.ability.name === CardAbility.SpawnEffectInRow
       "
     />
   </div>
@@ -59,8 +68,14 @@ import LockAbility from "@/components/UI/CardsUI/Cards/Abilities/LockAbility"
 import DestroyAbility from "@/components/UI/CardsUI/Cards/Abilities/DestroyAbility"
 import CardInteractionAbility from "@/components/UI/CardsUI/Cards/Abilities/CardInteractionAbility"
 import FieldInteractionAbility from "@/components/UI/CardsUI/Cards/Abilities/FieldInteractionAbility.vue"
+import { CardAbility } from "@/logic/models"
 export default {
   name: "card-ability-circle",
+  computed: {
+    CardAbility() {
+      return CardAbility
+    },
+  },
   components: {
     FieldInteractionAbility,
     CardInteractionAbility,
