0b36e1db4a7e2b1823cd5b03114db32b57a0d3b6
apodisation13 <apodisation13@gmail.com>
2026-02-06 16:04:12 +0300

    card abilities models

--- src/logic/player_move/passive_abilities/passives_leader/add-charges-to-leader-if-play-special.js
+++ src/logic/player_move/passive_abilities/passives_leader/add-charges-to-leader-if-play-special.js
@@ -1,5 +1,7 @@
+import { CardType } from "@/logic/models"
+
 function add_charges_to_leader_if_play_special(card, leader) {
-  if (card.type === "Special") {
+  if (card.type === CardType.Special) {
     leader.charges += 1
   }
 }
