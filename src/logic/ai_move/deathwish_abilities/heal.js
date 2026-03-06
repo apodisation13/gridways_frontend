b57e545fbaf31781d96dd4fc1371cbb704b522e2
apodisation13 <apodisation13@gmail.com>
2026-03-06 14:15:46 +0300

    animations rework

--- src/logic/ai_move/deathwish_abilities/heal.js
+++ src/logic/ai_move/deathwish_abilities/heal.js
@@ -1,6 +1,5 @@
 import { get_all_enemies } from "@/logic/player_move/service/service_for_player_move"
 import { sound_deathwish, sound_heal } from "@/logic/play_sounds"
-import { timeoutAnimationValue } from "@/logic/game_logic/timers"
 
 export function deathwish_heal_all(enemy, gameObj, timeout = 1000) {
   const { field, enemy_leader } = gameObj
@@ -9,13 +8,10 @@ export function deathwish_heal_all(enemy, gameObj, timeout = 1000) {
   sound_deathwish()
   sound_heal()
   all_enemies.forEach(e => {
-    timeoutAnimationValue(
-      e,
-      "hp",
-      `${e.hp}+${enemy.deathwish_value}`,
-      enemy.deathwish_value,
-      null,
-      timeout * 0.5
-    )
+    e.hp_delta = enemy.deathwish_value
+    setTimeout(() => {
+      e.hp_delta = null
+    }, timeout)
+    e.hp += enemy.deathwish_value
   })
 }
