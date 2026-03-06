15b3521bbc55b2803cd95215758153c16e11f032
apodisation13 <apodisation13@gmail.com>
2026-03-06 14:15:19 +0300

    animations rework

--- src/logic/ai_move/passive_abilities/passives_in_field/decrease_player_damage.js
+++ src/logic/ai_move/passive_abilities/passives_in_field/decrease_player_damage.js
@@ -1,21 +1,14 @@
 import { sound_enemy_decrease_player_damage } from "@/logic/play_sounds"
-import { choice } from "@/lib/utils"
-import {
-  timeoutAnimationFlag,
-  timeoutAnimationValue,
-} from "@/logic/game_logic/timers"
+import { choice_element } from "@/lib/utils"
 
 function decrease_player_damage(enemy, hand, timeout = 1000) {
-  let random = choice(hand)
-  timeoutAnimationValue(
-    hand[random],
-    "damage",
-    `${hand[random].damage}-${enemy.value}`,
-    enemy.value * -1,
-    sound_enemy_decrease_player_damage,
-    timeout * 0.5
-  )
-  timeoutAnimationFlag(enemy, "damages_player", null, timeout * 0.5)
+  let random_hand_card = choice_element(hand)
+  random_hand_card.dmg_delta -= enemy.value
+  sound_enemy_decrease_player_damage()
+  setTimeout(() => {
+    random_hand_card.dmg_delta = null
+  }, timeout * 0.5)
+  random_hand_card.damage -= enemy.value
 }
 
 export { decrease_player_damage }
