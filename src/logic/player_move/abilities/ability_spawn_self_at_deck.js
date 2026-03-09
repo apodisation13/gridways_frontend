f81757874e601fa8a6ff67ad4ff9da398547d361
apodisation13 <apodisation13@gmail.com>
2026-03-09 10:15:50 +0300

    fixes

--- src/logic/player_move/abilities/ability_spawn_self_at_deck.js
+++ src/logic/player_move/abilities/ability_spawn_self_at_deck.js
@@ -1,8 +1,13 @@
 import { copyObj } from "@/lib/utils"
 import { timeoutAnimationFlag } from "@/logic/game_logic/timers"
+import store from "@/store"
 
 export function spawn_self_at_deck(card, gameObj, timeout = 1000) {
-  gameObj.deck.push(copyObj(card))
+  let card_copy = copyObj(card)
+  const store_cards = store.getters["all_cards"]
+  const base_card = store_cards.find(card => card.card.id === card_copy.id)
+  card_copy.damage = base_card.card.damage
+  gameObj.deck.push(card_copy)
   timeoutAnimationFlag(
     gameObj.deck[0],
     "trigger_deck_passive",
