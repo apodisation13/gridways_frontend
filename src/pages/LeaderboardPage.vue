a5ca25896eb76d411f7905dbc7eb2f8804c2e64b
apodisation13 <apodisation13@gmail.com>
2026-03-12 13:56:51 +0300

    get and post stats

---
+++ src/pages/LeaderboardPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    bbb
+    <router-link :to="{ path: '/stats', query: { userId: 39 } }">
+      Регистрация
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeaderboardPage",
+}
+</script>
+
+<style scoped></style>
