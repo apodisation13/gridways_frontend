cb7c6a36420294208a71d16d83a390fcc18a372d
apodisation13 <apodisation13@gmail.com>
2026-03-23 16:28:19 +0300

    enemy status

---
+++ src/components/UI/CardsUI/Enemies/EnemyStatus.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="enemy-status"
+    :style="{
+      'background-image':
+        'url(' +
+        require(`@/assets/icons/enemy/enemy_status_${enemy.status}.svg`) +
+        ')',
+    }"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: {
+    enemy: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.enemy-status {
+  position: absolute;
+  bottom: 1%;
+  right: -2%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  width: 27%;
+  aspect-ratio: 1 / 1;
+  container-type: size;
+}
+</style>
