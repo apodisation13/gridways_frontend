636a062a31309a8ddb3435e493c9fbca8ac64ba5
apodisation13 <apodisation13@gmail.com>
2026-03-25 14:57:51 +0300

    enemy status

--- src/logic/player_move/abilities/ability_purify.js
+++ src/logic/player_move/abilities/ability_purify.js
@@ -2,7 +2,10 @@ export function purify(enemy) {
   enemy.deathwish = null
   enemy.has_deathwish = false
   enemy.passive_ability = null
-  enemy.has_passive = null
+  enemy.has_passive_in_field = false
+  enemy.has_passive_in_grave = false
+  enemy.has_passive_in_field = false
+  enemy.has_passive = false
   enemy.status = null
-  enemy.shield = null
+  enemy.shield = false
 }
