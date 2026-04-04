9d7d6cd66aea68533b3e0b4f862d89d958614227
apodisation13 <apodisation13@gmail.com>
2026-04-04 13:04:04 +0300

    passive values

--- src/logic/player_move/player_move.js
+++ src/logic/player_move/player_move.js
@@ -46,7 +46,8 @@ function damage_ai_card(card, enemy, gameObj) {
     heal(card)
   } else if (ability === CardAbility.DamageAll) {
     damage_all(field, card, gameObj, timeout)
-    if (enemy_leader.hp > 0) hit_one_enemy(enemy_leader, card, gameObj, timeout)
+    if (enemy_leader.data.hp > 0)
+      hit_one_enemy(enemy_leader, card, gameObj, timeout)
     setTimeout(
       () => check_win(field, enemies, enemy_leader, enemies_grave),
       timeout * 1.2
