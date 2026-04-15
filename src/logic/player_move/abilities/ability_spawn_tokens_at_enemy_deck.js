d9efb23a3c02c1053eb6e2f444c3b2d5b3bb196f
apodisation13 <apodisation13@gmail.com>
2026-04-15 13:48:14 +0300

    more fixes

--- src/logic/player_move/abilities/ability_spawn_tokens_at_enemy_deck.js
+++ src/logic/player_move/abilities/ability_spawn_tokens_at_enemy_deck.js
@@ -10,15 +10,15 @@ export function spawn_tokens_at_enemy_deck(card, enemy, gameObj) {
   token.data.hp = 1
   token.data.base.base_hp = 1
   token.data.damage = 1
-  token.data.deathwish = { value: card.data.value }
+  token.data.deathwish = { value: card.data.value } // это на сколько урон в руке потом увеличить
   token.data.value = card.data.value
   token.deathwish = {
     name: "incr-dmg-to-hand-by-value",
     description: "Увеличьте всем картам в РУКЕ урон на {deathwish_value}",
   }
 
-  // card.value раз положили этот токен врага в его колоду
-  for (let i = 0; i < card.data.value; i++) {
+  // card.data.tokens_number раз положили этот токен врага в его колоду
+  for (let i = 0; i < card.data.tokens_number; i++) {
     enemies.push(copyObj(token))
   }
 }
