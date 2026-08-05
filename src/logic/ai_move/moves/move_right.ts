eaf52fb84d88d3b64e4852317db3fb0bd989cad8
apodisation13 <apodisation13@gmail.com>
2026-08-05 12:52:38 +0300

    effects

--- src/logic/ai_move/moves/move_right.ts
+++ src/logic/ai_move/moves/move_right.ts
@@ -1,23 +1,19 @@
+import { applyEffectAtCell } from "@/logic/ai_move/effects_interaction"
 import { damage_player } from "@/logic/ai_move/moves/damage"
 import { sound_enemy_move_down } from "@/logic/play_sounds"
-import type { Enemy } from "@/types"
+import type { Enemy, GameObj } from "@/types"
 
 export function right_move(
   field: (Enemy | "")[],
   i: number,
+  gameObj: GameObj,
   timeout = 1000
 ): void {
   // враг, который стоит снизу справа, ячейка номер 11
-  if (i === 11) {
-    damage_player(field, i, timeout)
-    return
-  }
-
-  // ДЛЯ ОСТАЛЬНЫХ ВРАГОВ
-
   // ЕСЛИ У ВРАГА ЕСТЬ ВРАГ СПРАВА ОТ НЕГО (то есть индекс + 1)
-  if (field[i + 1]) {
-    damage_player(field, i, timeout)
+  if (i === 11 || field[i + 1]) {
+    const killed = applyEffectAtCell(i, gameObj, timeout)
+    if (!killed) damage_player(field, i, timeout)
     return
   }
 
@@ -25,4 +21,5 @@ export function right_move(
   field[i + 1] = field[i] // враг сдвинулся правее, или на следующую строку слева если он был в правом столбце
   field[i] = ""
   sound_enemy_move_down()
+  applyEffectAtCell(i + 1, gameObj, timeout)
 }
--- src/logic/ai_move/moves/move_stand_still.ts
+++ src/logic/ai_move/moves/move_stand_still.ts
@@ -1,10 +1,13 @@
+import { applyEffectAtCell } from "@/logic/ai_move/effects_interaction"
 import { damage_player } from "@/logic/ai_move/moves/damage"
-import type { Enemy } from "@/types"
+import type { Enemy, GameObj } from "@/types"
 
 export function stand_still(
   field: (Enemy | "")[],
   i: number,
+  gameObj: GameObj,
   timeout = 1000
 ): void {
-  damage_player(field, i, timeout)
+  const killed = applyEffectAtCell(i, gameObj, timeout)
+  if (!killed) damage_player(field, i, timeout)
 }
