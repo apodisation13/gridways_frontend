628417e1807e85e04a306ad56f4ae6384081b454
apodisation13 <apodisation13@gmail.com>
2026-08-29 12:26:48 +0300

    enemy leader abilities

--- src/logic/ai_move/ai_move.ts
+++ src/logic/ai_move/ai_move.ts
@@ -5,11 +5,10 @@ import { random_move } from "@/logic/ai_move/moves/move_random"
 import { right_move } from "@/logic/ai_move/moves/move_right"
 import { move_row } from "@/logic/ai_move/moves/move_row"
 import { stand_still } from "@/logic/ai_move/moves/move_stand_still"
-import { check_lose } from "@/logic/ai_move/service/check_lose"
 import { set_already_jumped } from "@/logic/ai_move/service/service_for_ai_move"
 import { get_all_enemies } from "@/logic/player_move/service/service_for_player_move"
 import store from "@/store"
-import { Enemy, EnemyLeaderAbilityEnum, GameObj } from "@/types"
+import { Enemy, GameObj } from "@/types"
 import { EnemyMove } from "@/types"
 
 export function ai_move(gameObj: GameObj, timeout = 1000): void {
@@ -50,24 +49,4 @@ export function ai_move(gameObj: GameObj, timeout = 1000): void {
       i += 1
     }
   }, timeout)
-}
-
-// эта функция срабатывает для лидера врагов только в начале игры 1 раз
-export function enemy_leader_ai_move_once(gameObj: GameObj): void {
-  const { enemy_leader, deck } = gameObj
-  const ela = enemy_leader.ability?.name
-
-  if (!ela) return // есть лидеры у кого абилки нет
-
-  if (ela === EnemyLeaderAbilityEnum.DamageOnce) {
-    const value = enemy_leader.data?.value || 0
-    store.commit("change_health", -value)
-    check_lose()
-  } else if (ela === EnemyLeaderAbilityEnum.DecreaseAllPlayerDamage) {
-    const value = enemy_leader.data?.value || 0
-    deck.forEach(card => {
-      card.data.damage -= value
-      if (card.data.damage < 0) card.data.damage = 0
-    })
-  }
 }
--- src/mixins/GamePage/startgame.ts
+++ src/mixins/GamePage/startgame.ts
@@ -1,6 +1,6 @@
 import { defineComponent } from "vue"
 
-import { enemy_leader_ai_move_once } from "@/logic/ai_move/ai_move"
+import { enemy_leader_ai_move_once } from "@/logic/ai_move/ai_leader_move_once"
 import { draw_hand } from "@/logic/game_logic/draw_hand"
 import { place_enemies } from "@/logic/game_logic/place_enemies"
 import { passives_upon_beginning } from "@/logic/player_move/passive_abilities/passives_upon_beginning"
@@ -27,10 +27,10 @@ export default defineComponent({
         JSON.stringify(this.$store.state.game.level.enemies)
       )
 
-      // рандомно расставит врагов по полю, изменяет поле и врагов
-      place_enemies(this.gameObj.field, this.gameObj.enemies)
       // АБИЛКИ ЛИДЕРА врага в самом начале
       enemy_leader_ai_move_once(this.gameObj)
+      // рандомно расставит врагов по полю, изменяет поле и врагов
+      place_enemies(this.gameObj.field, this.gameObj.enemies)
       // пассивные способности карт, которые должны срабатывать 1 раз в начале игры
       passives_upon_beginning(this.gameObj)
       // вытянет руку, остальное оставит в деке, изменяет руку и деку
